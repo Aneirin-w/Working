1094d98b4994dd18eeda739f54733780b74c22d3
Aneirinw <aneirinwee@gmail.com>
2023-09-11 23:00:36 +0100

    Change Hello World

--- Working/main.cpp
+++ Working/main.cpp
@@ -9,6 +9,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello World!\n";
+    std::cout << "This is a test! I am changing stuff\n";
     return 0;
 }
