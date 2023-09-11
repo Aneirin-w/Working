5fd3b0f71c905c7f5239076d27e6e4b9fa04a6f9
Aneirinw <aneirinwee@gmail.com>
2023-09-11 22:57:24 +0100

    Initial Commit

---
+++ Working/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Working
+//
+//  Created by Aneirin Wee on 11/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello World!\n";
+    return 0;
+}
