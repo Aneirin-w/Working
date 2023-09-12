c9a4c3f335a61fb69fb4442e7bd869383ca4d357
Aneirinw <aneirinwee@gmail.com>
2023-09-12 13:32:41 +0100

    reset for inc-dec-op

--- Working/main.cpp
+++ Working/main.cpp
@@ -1,5 +1,5 @@
-// operators.cpp by Bill Weinman [bw.org]
-// updated 2022-06-17
+// working.cpp by Bill Weinman [bw.org]
+// updated 2022-05-21
 #include <format>
 #include <iostream>
 
@@ -7,39 +7,5 @@ using std::format;
 using std::cout;
 
 int main() {
-    auto x = 5;
-    auto y = 47;
-    cout << format("x is {}\n", x);
-    cout << format("y is {}\n\n", y);
-    
-    bool a = true;
-    auto b = false;
-    bool c = true;
-    
-    cout << format("a is {}\n", a);
-    cout << format("b is {}\n", b);
-    cout << format("size of c is {} bits\n", sizeof(c) * 8);
-
-    // Bool type is usually 8 bits (see c)
-    
-    std::cout << "\n";
-    
-    if (a && b ) cout << "true\n";
-    else cout << "flase\n";
-    
-    if (a || b ) cout << "true\n";
-    else cout << "flase\n";
-    
-    if (!a ) cout << "true\n";
-    else cout << "flase\n";
-    
-    // and = &&
-    // or = ||
-    // not = !      eg not a = !a
-    
-    std::cout << "\n";
-    if ( 42 > 5 || a ) cout << "true\n";
-    else cout << "flase\n";
-// first condiiton is satisfied, second condition is not operated 
-    
+    cout << "Hello, World!\n";
 }
